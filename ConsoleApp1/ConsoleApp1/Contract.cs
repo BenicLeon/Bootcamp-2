96bcb9c712328ca82d69f38605bc33be4b2982da
Leon <leon.benic1@gmail.com>
2024-05-28 10:51:33 +0200

    Added Contract.cs and IContract.cs

---
+++ ConsoleApp1/ConsoleApp1/Contract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Contract : IContract
+    {
+        public DateTime SigningDate { get; set; }
+        public decimal Salary { get; set; }
+
+        public bool IsSigned { get; set; }
+
+        public Contract(DateTime signingDate, decimal salary)
+        {
+            SigningDate = signingDate;
+            Salary = salary;
+            IsSigned = false;
+        }
+
+        public void SignContract()
+        {
+            IsSigned = true;
+            Console.WriteLine($"Contract signed on {SigningDate.ToShortDateString()} with a salary of {Salary:C}.");
+        }
+
+        public void TerminateContract()
+        {
+            IsSigned = false;
+            Console.WriteLine("Contract terminated.");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/IContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface IContract
+    {
+        bool IsSigned
+        {
+            get;
+            set;
+        }
+        void SignContract();
+        void TerminateContract();
+    }
+}
